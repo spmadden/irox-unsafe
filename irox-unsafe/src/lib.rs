19126e66b3c7af472eb69dd9ad4c0b5a35c3f817
Sean P. Madden <sean@seanmadden.net>
2024-05-18 19:53:04 -0400

    refactor(irox): rename exports to be feature names

--- irox-unsafe/src/lib.rs
+++ irox-unsafe/src/lib.rs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright 2023 IROX Contributors
 
+#[cfg(feature = "safe-linux")]
+pub use irox_safe_linux as safe_linux;
 #[cfg(feature = "safe-windows")]
-pub use irox_safe_windows;
+pub use irox_safe_windows as safe_windows;
